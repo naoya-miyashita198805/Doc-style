28a2b7c031d0e46a9507a9c1148d5bd2a9a0101b
naoya-miyashita198805 <naoya.miyashita1988@gmail.com>
2020-02-10 21:30:09 +0900

    お気に入りブラッシュアップ

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,12 +3,28 @@ class BookmarksController < ApplicationController
   def create
     bookmark = current_user.bookmarks.build(document_id: params[:document_id])
     bookmark.save!
-    redirect_to documents_path, success: t('.flash.bookmark')
+    # redirect_to documents_path, success: t('.flash.bookmark')
+    # if request.referer.include?("searches")
+      #前のページが指定したURLだった場合のリンク先
+    redirect_back(fallback_location: root_path)
+      
+      #その他のリンク先
+    # else  
+      # redirect_to root_path
+    # end
   end
 
   def destroy
     current_user.bookmarks.find_by(document_id: params[:document_id]).destroy!
-    redirect_to documents_path, success: t('.flash.not_bookmark')
+    # redirect_to documents_path, success: t('.flash.not_bookmark')
+    # if request.referer.include?("searches")
+      #前のページが指定したURLだった場合のリンク先
+    redirect_back(fallback_location: root_path)
+      
+      #その他のリンク先
+    # else  
+      # redirect_to root_path
+    # end
   end
 
 end
